a88e1fa46ac43af0571667c525ebf2138e3731c0
naveen1000 <loki.naveen1000@gmail.com>
2020-07-23 12:40:00 +0530

    make it better3"

--- cronjob.py
+++ cronjob.py
@@ -2,10 +2,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Main cronjob function.
-from main import cronjob
+from main import cronjob,cronjob2
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
 scheduler.add_job(cronjob, "interval", seconds=30)
-
+scheduler.add_job(cronjob2, "interval", seconds=50)
 scheduler.start()
--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def cronjob():
     """
     print("Cron job is running")
     print("Tick! The time is: %s" % datetime.now())
+
+def cronjob2():
+    """
+    Main cron job.
+    The main cronjob to be run continuously.
+    """
+    print("Cron job2 is running")
+    print("Tick! The time is: %s" % datetime.now())
