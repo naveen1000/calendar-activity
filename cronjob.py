f44fcb47f3b4fa7e4a95b77db9f313f03e9a759f
naveen1000 <loki.naveen1000@gmail.com>
2020-08-04 12:19:19 +0530

    cron

--- cronjob.py
+++ cronjob.py
@@ -7,7 +7,10 @@ from main import cronjob
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
 
-scheduler.add_job(cronjob, 'cron', hour='6', minute='44')
+scheduler.add_job(cronjob, 'cron', hour='7', minute='45')
+scheduler.add_job(cronjob, 'cron', hour='13', minute='0')
+scheduler.add_job(cronjob, 'cron', hour='15', minute='30')
+
 #scheduler.add_job(cronjob, "interval", seconds=3600*4)
 #scheduler.add_job(cronjob2, "interval", seconds=50)
 scheduler.start()
