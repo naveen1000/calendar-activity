b2d993f85ca589b915c461ce48860fadd5e594b7
naveen1000 <loki.naveen1000@gmail.com>
2020-08-04 11:52:21 +0530

    cron change

--- cronjob.py
+++ cronjob.py
@@ -1,12 +1,12 @@
 # Package Scheduler.
-#from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.multi import OrTrigger
 
 # Main cronjob function.
 from main import cronjob
 trigger = OrTrigger([CronTrigger(hour=1, minute=30), CronTrigger(hour=23, minute=10)]
 # Create an instance of scheduler and add function.
-#scheduler = BlockingScheduler()
+scheduler = BlockingScheduler()
 scheduler.add_job(cronjob, trigger)
 
 #scheduler.add_job(cronjob2, "interval", seconds=50)
